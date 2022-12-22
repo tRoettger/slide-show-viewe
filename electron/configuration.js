08e2d7f1f0669d1b836891b21e3148be1fc1add5
tRoettger <tobias.roettger@gmx.net>
2022-12-22 12:26:02 +0100

    Open configuration view in new window

--- electron/configuration.js
+++ electron/configuration.js
@@ -1,3 +1,16 @@
+const { BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+const CONFIG_WINDOW_PROPERTIES = {
+    width: 640, height: 480,
+    webPreferences: { nodeIntegration: false, contextIsolation: false },
+    autoHideMenuBar: true
+};
+
 exports.configureApp = () => {
-    console.log("configure app");
+    const configWindow = new BrowserWindow(CONFIG_WINDOW_PROPERTIES);
+    configWindow.loadFile("public/slide-show-config.html");
+    if(isDev) {
+        configWindow.webContents.openDevTools({ mode: "detach" });
+    }
 };
