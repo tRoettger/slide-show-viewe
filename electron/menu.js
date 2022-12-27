439302fb9375b5425337f2bf061c13e148811c92
tRoettger <tobias.roettger@gmx.net>
2022-12-27 16:48:55 +0100

    Add slideshow controlls to menu

--- electron/menu.js
+++ electron/menu.js
@@ -18,8 +18,12 @@ const MENU_TEMPLATE = [
     }, {
         label: "Diashow",
         submenu: [
+            { "role": "start-slide-show", "label": "Diashow starten/stoppen", "accelerator": "Space", click: controller.startSlideShow }
             { "role": "cfg-app", "label": "Einstellungen", "accelerator": "Ctrl+P", click: configureApp },
-            { "role": "load-cfg", "label": "Gespeicherte Einstellungen laden", "accelerator": "Ctrl+L", click: loadConfig }
+            { "role": "load-cfg", "label": "Gespeicherte Einstellungen laden", "accelerator": "Ctrl+L", click: loadConfig },
+            { "type": "separator" },
+            { "role": "next", "label": "vorheriges Bild", "accelerator": "Left", click: controller.gotoPreviousImage },
+            { "role": "next", "label": "nächstes Bild", "accelerator": "Right", click: controller.gotoNextImage }
         ]
     }, {
         label: "Ansicht",
