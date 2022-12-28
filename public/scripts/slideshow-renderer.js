b90d58d632f0b93db9bf856249f28741c026b315
tRoettger <tobias.roettger@gmx.net>
2022-12-28 21:14:17 +0100

    Add rendering of controls

--- public/scripts/slideshow-renderer.js
+++ public/scripts/slideshow-renderer.js
@@ -1,10 +1,22 @@
 const CSS_ROOT = document.querySelector(":root").style;
 const ROOT = document.getElementById("root");
+const BTN_SLIDESHOW = document.getElementById("slideshow-btn");
+const EMPTY_INTERVAL = setInterval(() => {}, 5000);
+const SYMBOL = {
+    start: "&#9655;",
+    pause: "&#10073;&#10073;"
+};
 
 class SlideshowRenderer {
-    constructor(cssRoot, display) {
+    constructor(cssRoot, display, playBtn) {
         this.cssRoot = cssRoot;
         this.display = display;
+        this.playBtn = playBtn;
+        this.interval = EMPTY_INTERVAL;
+    }
+
+    addImageElements(count) {
+        this.createImageElements(count).forEach(this.display.appendChild);
     }
 
     updateAnimation(config) {
@@ -13,9 +25,9 @@ class SlideshowRenderer {
         this.cssRoot.setProperty("--timing-function", config.timingFunction);
     }
 
-    createImageElements() {
+    createImageElements(count) {
         var imageElements = [];
-        for(var i = this.count - 1; i >= 0; i--) {
+        for(var i = count - 1; i >= 0; i--) {
             var image = document.createElement("img");
             image.id = "album-image-" + i;
             image.className = "album-image";
@@ -33,6 +45,42 @@ class SlideshowRenderer {
             this.display.removeChild(this.display.lastChild); 
         }
     };
+
+    showNext() {
+        var child = this.display.lastChild;
+        child.style.animationName = "none";
+        this.display.removeChild(child);
+        this.display.insertBefore(child, this.display.firstChild);
+    }
+
+    showPrevious() {
+        this.display.lastChild.style.animationName = "none";
+        var child = this.display.firstChild;
+        this.display.removeChild(child);
+        this.display.appendChild(child);
+    }
+    
+    createTransition() {
+        this.display.lastChild.style.animationName = "fade";
+    }
+
+    startTransition(swapDuration, onFinish) {
+        playBtn.innerHTML = SYMBOL.pause;
+        this.interval = setInterval(() => this.swapInterval(onFinish), swapDuration * 1000);
+    }
+
+    swapInterval(onFinish) {
+        onFinish();
+        createTransition();
+    };
+
+    stopTransition() {
+        playBtn.innerHTML =  SYMBOL.start;
+        clearInterval(this.interval);
+        if(this.display.firstChild) {
+            this.display.lastChild.style.animationName = "none";
+        }
+    }
 }
 
-exports.slideshowRenderer = new SlideshowRenderer(CSS_ROOT, ROOT);
+exports.slideshowRenderer = new SlideshowRenderer(CSS_ROOT, ROOT, BTN_SLIDESHOW);
