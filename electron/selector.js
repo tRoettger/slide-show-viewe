27197bc685d2f469765b8115879c7787bf1e346e
tRoettger <tobias.roettger@gmx.net>
2022-12-31 11:53:24 +0100

    Load selector/view.html

--- electron/selector.js
+++ electron/selector.js
@@ -1,3 +1,11 @@
+const { BrowserWindow } = require("electron");
+
+const SELECTOR_WINDOW_PROPERTIES = {
+    width: 640, height: 480,
+    webPreferences: { nodeIntegration: true, contextIsolation: false },
+    autoHideMenuBar: true
+};
+
 class AlbumSelector {
     constructor() {
         this.openWindow = this.openWindow.bind(this);
@@ -5,6 +13,8 @@ class AlbumSelector {
     }
 
     openWindow() {
+        this.window = new BrowserWindow(SELECTOR_WINDOW_PROPERTIES);
+        this.window.loadFile("public/selector/view.html");
         console.log("Open selector window: ", this.test);
     }
 }
