8f058a4a1d1409a5724b5eba2c92beb671e8bfe5
tRoettger <tobias.roettger@gmx.net>
2023-01-02 20:39:27 +0100

    Clear display before requesting filtered albums
    The backend will send the filtered albums of the first page when a filter is received

--- public/selector/event-listener.js
+++ public/selector/event-listener.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require("electron");
 const { Channel, FilterType } = require("../../shared/communication");
+const { albumRenderer } = require("./album-renderer");
 
 const FORM_FILTER = document.getElementById("filter-form");
 const INPUT_FILTER = document.getElementById("filter-input");
@@ -7,6 +8,7 @@ const SELECT_ORDER = document.getElementById("order");
 
 FORM_FILTER.addEventListener("submit", e => {
     e.preventDefault();
+    albumRenderer.clearDisplay();
     ipcRenderer.send(Channel.FILTER_ALBUMS, {
         type: FilterType.NAME,
         value: INPUT_FILTER.value
