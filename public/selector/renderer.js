41e73c7c0026f7dba41985d15032a96683f2dd53
tRoettger <tobias.roettger@gmx.net>
2023-01-02 20:49:23 +0100

    Require event-listener in renderer
    Instead of loading it within html.

--- public/selector/renderer.js
+++ public/selector/renderer.js
@@ -2,6 +2,7 @@ const { ipcRenderer } = require("electron");
 const { Channel } = require("../../shared/communication");
 const { albumRenderer } = require("./album-renderer");
 const { paginationRenderer } = require("./pagination-renderer");
+require("./event-listener")
 
 ipcRenderer.on(Channel.NOTIFY_ALBUM, (event, album) => {
     albumRenderer.render(album);
@@ -9,4 +10,4 @@ ipcRenderer.on(Channel.NOTIFY_ALBUM, (event, album) => {
 
 ipcRenderer.on(Channel.NOTIFY_PAGE_INFO, (event, pageInfo) => {
     paginationRenderer.render(pageInfo);
-}); 
+});
