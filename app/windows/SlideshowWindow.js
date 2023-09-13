86d4c37e23773be4d2b973a20feb833dd7778595
tRoettger <tobias.roettger@gmx.net>
2023-09-13 13:51:56 +0200

    #20 closes config and selection if main window is closed

--- app/windows/SlideshowWindow.js
+++ app/windows/SlideshowWindow.js
@@ -1,11 +1,17 @@
 const path = require('path');
 const { saveWindowProperties, readWindowProperties } = require("../services/SlideshowConfigurer");
 const { BrowserWindow } = require('electron');
+const configWindow = require('./SlideshowConfigWindow');
+const selectionWindow = require('./AlbumSelectionWindow');
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow(readWindowProperties());
   mainWindow.loadFile(path.join("app", "renderer", "pages", "slideshow.html"));
-  mainWindow.on("close", () => saveWindowProperties(mainWindow));
+  mainWindow.on("close", () => {
+    saveWindowProperties(mainWindow);
+    configWindow.ifPresent(window => window.close());
+    selectionWindow.ifPresent(window => window.close());
+  });
   return mainWindow;
 };
 
