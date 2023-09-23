8369af7741ea47868e5dcb3a60ebbf7f3c16ea8f
tRoettger <tobias.roettger@gmx.net>
2023-09-23 15:24:38 +0200

    #30 fixes showNext before transition ended

--- app/renderer/pages/slideshow-controller.js
+++ app/renderer/pages/slideshow-controller.js
@@ -33,10 +33,10 @@
         }
 
         pause() {
-            this.running = false;
             if(this.interval) {
                 clearInterval(this.interval);
             }
+            this.running = false;
             this.onPause();
         }
 
@@ -53,9 +53,14 @@
             if(!this.isRunning() && this.getCount() > 1) {
                 this.running = true;
                 this.onPlay();
-                let swapDuration = (this.config.viewDuration + this.config.transitionDuration) * 1000;
+                const viewDuration = this.config.viewDuration * 1000;
+                const transitionDuration = this.config.transitionDuration * 1000;
+                const swapDuration = viewDuration + transitionDuration;
                 console.log("Swap duration", swapDuration);
-                this.remoteControl.transition();
+
+                setTimeout(() => {
+                    this.remoteControl.transition();
+                }, viewDuration);
                 this.interval = setInterval(() => {
                     this.showNext();
                     this.remoteControl.transition();
@@ -78,13 +83,23 @@
         BTN_SLIDESHOW.innerHTML =  "&#10073;&#10073;";
         BTN_SLIDESHOW.title = "Diashow pausieren";
     };
+    
+    const wrapWithStopAndStart = (stepping, controller) => {
+        if(controller.isRunning()) {
+            controller.pause();
+            stepping();
+            controller.start();
+        } else {
+            stepping();
+        }
+    };
 
     let controller;
 
     configApi.subscribe(ID, (config) => {
         if(controller) {
             console.log("controller received config", config);
-            controller.configure(config);
+            wrapWithStopAndStart(() => controller.configure(config), controller);
         } else {
             controller = new SlideshowController(config, api.controlSlideshow, onPause, onPlay);
     
@@ -96,25 +111,11 @@
                 }
             };
     
-            const wrapWithStopAndStart = (stepping) => {
-                if(controller.isRunning()) {
-                    controller.pause();
-                    stepping();
-                    controller.start();
-                } else {
-                    stepping();
-                }
-            };
-    
             BTN_SLIDESHOW.addEventListener("click", controlSlideshow);
-            BTN_NEXT.addEventListener("click", (e) => wrapWithStopAndStart(controller.showNext));
-            BTN_PREVIOUS.addEventListener("click", (e) => wrapWithStopAndStart(controller.showPrevious));
+            BTN_NEXT.addEventListener("click", (e) => wrapWithStopAndStart(controller.showNext, controller));
+            BTN_PREVIOUS.addEventListener("click", (e) => wrapWithStopAndStart(controller.showPrevious, controller));
     
-            api.subscribeAlbum(ID, album => {
-                console.log("Received album: ", album);
-                controller.loadAlbum(album);
-            });
-
+            api.subscribeAlbum(ID, album => controller.loadAlbum(album));
             api.controlSlideshow.subscribeStart(ID, controller.start);
             api.controlSlideshow.subscribeStop(ID, controller.pause);
         }
